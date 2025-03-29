98eb4529f3e11a23df68f3bebecf3271f875b21a
Jonas Pflanzer <noreply@example.com>
2025-03-29 11:10:55 +0100

    Import Interpreter module to shorten expressions

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,13 @@
-(** let prog: Appendix_c_verifier.Interpreter.expression = Let("x", Bool(false), Cond(Id("x"), Num(42), Num(1337))) *)
-let prog: Appendix_c_verifier.Interpreter.expression = Let("x", Malloc([Num(-6); Num(5)]), BinOp(Add, Mget(Id("x")), Mget(BinOp(Add, Id("x"), Loc(1)))))
-let env: Appendix_c_verifier.Interpreter.environment = Appendix_c_verifier.Interpreter.EnvironmentMap.empty
-let h: Appendix_c_verifier.Interpreter.heap = Appendix_c_verifier.Interpreter.HeapMap.empty
+open Appendix_c_verifier.Interpreter
+
+(** let prog: expression = Let("x", Bool(false), Cond(Id("x"), Num(42), Num(1337))) *)
+let prog: expression = Let("x", Malloc([Num(-6); Num(5)]), BinOp(Add, Mget(Id("x")), Mget(BinOp(Add, Id("x"), Loc(1)))))
+let env: environment = EnvironmentMap.empty
+let h: heap = HeapMap.empty
 
 let () = print_endline "Appendix C Verifier"
 let () =
-  let (res, _) = Appendix_c_verifier.Interpreter.interp prog env h in
+  let (res, _) = interp prog env h in
     match res with
     | Loc(l) -> print_string "Loc("; print_int l; print_endline ")"
     | Num(n) -> print_string "Num("; print_int n; print_endline ")"
