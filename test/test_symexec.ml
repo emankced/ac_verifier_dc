66c8ebb5e75d7ce3bf886451fe4ee3dff948fc33
Jonas Pflanzer <noreply@example.com>
2025-09-15 12:45:53 +0200

    Symexec tests use the new assert behavior
    (Assert no longer has a command and result always references the
    previous command's result.)

--- test/test_symexec.ml
+++ test/test_symexec.ml
@@ -7,112 +7,123 @@ let vf (src: string): bool =
     | _ -> false
 
 let%test "Verify arithmetics with itself 1" =
-  vf "@ result == 3 + 4 - 1 * 8 / 3 + 1 @
-      3 + 4 - 1 * 8 / 3 + 1"
+  vf "3 + 4 - 1 * 8 / 3 + 1;
+      @ result == 3 + 4 - 1 * 8 / 3 + 1 @"
 
 let%test "Verify arithmetics with itself 2" =
-  not (vf "@ result == 3 + 4 - 1 * 8 / 3 + 1 @
-          3 + 4 - 1 * 8 / 3")
+  not (vf "3 + 4 - 1 * 8 / 3;
+           @ result == 3 + 4 - 1 * 8 / 3 + 1 @")
 
 let%test "Verify arithmetics with itself 3" =
-  not (vf "@ result < 3 + 4 - 1 * 8 / 3 + 1 @
-          3 + 4 - 1 * 8 / 3 + 1")
+  not (vf "3 + 4 - 1 * 8 / 3 + 1;
+           @ result < 3 + 4 - 1 * 8 / 3 + 1 @")
 
 let%test "Verify simple arithmetics" =
-  vf "@ 18 == result @
-      let x := 5 in
-      1 + 2 + 6 + 4 + x"
+  vf "(let x := 5 in
+      1 + 2 + 6 + 4 + x);
+      @ 18 == result @"
 
 let%test "Verify mixed asserts 1" =
-  vf "@ result @
-      let x := true in
+  vf "let x := true in
       (let y := 5 in
-          @ result == 12 && y == 5 || (false == true) && false @
-          y + 7);
-      x"
+        y + 7;
+        @ result == 12 && y == 5 || (false == true) && false @);
+      x;
+      @ result @"
 
 let%test "Verify mixed asserts 2" =
-  vf "@ result @
-      let x := true in
+  vf "let x := true in
       (let y := 5 in
-          @ result == 12 && y > 4 || (false == true) && false @
-          y + 7);
-      x"
+        y + 7;
+        @ result == 12 && y > 4 || (false == true) && false @);
+      x;
+      @ result @"
 
 let%test "Verify mixed asserts 3" =
-  not (vf "@ result @
-          let x := true in
+  not (vf "let x := true in
           (let y := 5 in
-              @ result == 12 && y <= 4 || (false == true) && false @
-              y + 7);
-          x")
+            y + 7;
+            @ result == 12 && y <= 4 || (false == true) && false @);
+          x;
+          @ result @")
 
 let%test "Verify conditions 1" =
-  vf "@ result == 10 @
-      let x := 2 in
-        @ result >= 5 @
-        if x > 4 then
+  vf "(let x := 2 in
+        (if x > 4 then
+          5;
           @ result < 7 @
-          5
         else
+          10;
           @ result >= 7 @
-          10"
+        );
+        @ result >= 5 @
+      );
+      @ result == 10 @"
 
 let%test "Verify conditions 2" =
-  vf "@ result == 10 @
-      let x := 2 in
-        @ result > 5 @
-        if x > 4 then
+  vf "(let x := 2 in
+        (if x > 4 then
+          5;
           @ result < 7 @
-          5
         else
+          10;
           @ result >= 7 @
-          10"
+        );
+        @ result > 5 @
+      );
+      @ result == 10 @"
 
 let%test "Verify conditions 3" =
-  vf "@ result == 5 @
-      let x := 42 in
-        @ result >= 5 @
-        if x > 4 then
+  vf "(let x := 42 in
+        (if x > 4 then
+          5;
           @ result < 7 @
-          5
         else
+          10;
           @ result >= 7 @
-          10"
+        );
+        @ result >= 5 @
+      );
+      @ result == 5 @"
 
 let%test "Verify conditions 4" =
-  not (vf "@ result >= 5 @
-          let x := 42 in
-            @ result > 5 @
-            if x > 4 then
+  not (vf "(let x := 42 in
+            (if x > 4 then
+              5;
               @ result < 7 @
-              5
             else
+              10;
               @ result >= 7 @
-              10")
+            );
+            @ result > 5 @
+          );
+          @ result >= 5 @")
 
 let%test "Verify conditions 5" =
-  vf "@ result == 10 @
-      let x := 2 in
-        if true then x + 8 else 8 + x"
+  vf "(let x := 2 in
+        if true then x + 8 else 8 + x
+      );
+      @ result == 10 @"
 
-let%test "Verify aliasing 1" =
-  vf "@ result == 10 @
-      let x := 2 in
+let%test "Verify shadowing 1" =
+  vf "(let x := 2 in
         let x := 10 in
-          x"
+          x
+      );
+      @ result == 10 @"
 
-let%test "Verify aliasing 2" =
-  vf "@ result == 14 @
-      let x := 2 in
+let%test "Verify shadowing 2" =
+  vf "(let x := 2 in
       let y := x + x in
       let x := 10 in
       let y := x + y in
-        y"
+        y
+      );
+      @ result == 14 @"
 
-let%test "Verify aliasing 3" =
+let%test "Verify shadowing 3" =
   vf "let x := 2 in
-        @ x == 2 @
+        @ x == 2 @;
         let x := 10 in
-          @ x == 10 @
-            x"
+          @ x == 10 @;
+          x"
