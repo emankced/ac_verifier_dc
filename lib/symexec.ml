71daf88c8777c88b9071e10f223a443e3269bd56
Jonas Pflanzer <noreply@example.com>
2025-11-29 13:25:25 +0100

    Use premise for assert

--- lib/symexec.ml
+++ lib/symexec.ml
@@ -299,7 +299,8 @@ and symexec (expr: expression) (env: environment) (sdef: struct_definitions) (re
     let assert_env = if res == Unit then env else env |> StringMap.add "result" res in
     let premise = get_premise assert_env sdef h in
     let assertion = derive assertion assert_env sdef h in
-      solve [premise; assertion];
+    let formula = Z3.Boolean.mk_implies ctx premise assertion in
+      solve [premise; formula];
       k res h env sdef
 | Seq(_i, expr0, expr1) ->
     let k = (fun (res: value) (h: heap) _ _ ->
